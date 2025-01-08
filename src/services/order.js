a648b0f80a61955bd17ad22b87f3c9235ac2fc64
alfo0924 <halo0924@yahoo.com.tw>
2025-01-08 19:36:49 +0800

    checkoutView.vue 到結帳畫面待修

--- src/services/order.js
+++ src/services/order.js
@@ -1,6 +1,6 @@
 import {api} from '@/utils/axios'
 
- async function createOrder(shippingAddress, bearToken) {
+async function createOrder(shippingAddress, bearToken) {
     const response = await api.post('/orders',
         {
             shippingAddress: shippingAddress
@@ -10,12 +10,12 @@ import {api} from '@/utils/axios'
                 Authorization: `Bearer ${bearToken}`,
             },
         })
-    .catch(function (error) {
-        console.log(error.toJSON());
-    });
+        .catch(function (error) {
+            console.log(error.toJSON());
+        });
     const data = await response.data;
     console.log(data)
-  return data;
+    return data;
 }
 
 async function orderCreditPayment(orderId, cardNumber, bearToken){
@@ -29,12 +29,12 @@ async function orderCreditPayment(orderId, cardNumber, bearToken){
                 Authorization: `Bearer ${bearToken}`,
             },
         })
-    .catch(function (error) {
-        console.log(error.toJSON());
-    });
+        .catch(function (error) {
+            console.log(error.toJSON());
+        });
     const data = await response.data;
     console.log(data)
-  return data;
+    return data;
 }
 
 async function orderApplePayPayment(orderId, applePayToken, bearToken){
@@ -48,12 +48,12 @@ async function orderApplePayPayment(orderId, applePayToken, bearToken){
                 Authorization: `Bearer ${bearToken}`,
             },
         })
-    .catch(function (error) {
-        console.log(error.toJSON());
-    });
+        .catch(function (error) {
+            console.log(error.toJSON());
+        });
     const data = await response.data;
     console.log(data)
-  return data;
+    return data;
 }
 
 async function getOrder(bearToken){
@@ -63,12 +63,12 @@ async function getOrder(bearToken){
                 Authorization: `Bearer ${bearToken}`,
             },
         })
-    .catch(function (error) {
-        console.log(error.toJSON());
-    });
+        .catch(function (error) {
+            console.log(error.toJSON());
+        });
     const data = await response.data;
     console.log(data.data.orders)
-  return data.data.orders;
+    return data.data.orders;
 }
 
 async function getOrderDetail(orderId ,bearToken){
@@ -78,12 +78,12 @@ async function getOrderDetail(orderId ,bearToken){
                 Authorization: `Bearer ${bearToken}`,
             },
         })
-    .catch(function (error) {
-        console.log(error.toJSON());
-    });
+        .catch(function (error) {
+            console.log(error.toJSON());
+        });
     const data = await response.data;
     console.log(data.data)
-  return data.data;
+    return data.data;
 }
 
 export {createOrder, orderCreditPayment, orderApplePayPayment, getOrder, getOrderDetail}
